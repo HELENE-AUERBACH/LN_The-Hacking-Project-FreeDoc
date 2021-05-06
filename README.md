fd24f8799b7a82efea3302910ba5963f5161070b
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-05-06 20:47:26 +0200

    Ajout des fichiers "README.md", "db/development.sqlite3" et "db/seeds.rb".

--- README.md
+++ README.md
@@ -6,19 +6,36 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+ruby 2.7.1p83 (2020-03-31 revision a0c7c23c9c) [x86_64-linux]
 
-* System dependencies
-
-* Configuration
-
-* Database creation
+* Rails version
+Rails 5.2.3
 
 * Database initialization
+$ bundle install
+...
+$ rails db:seed
+3 docteurs créés :
+ID | FIRST_NAME | LAST_NAME | SPECIALTY     | ZIP_CODE  
+---|------------|-----------|---------------|-----------
+1  | Carmen     | Wehner    | Rhumatologie  | 36614-5292
+2  | Jasmin     | Roob      | Ophtalmologie | 32768-9554
+3  | Winnie     | Johnson   | Gynécologie   | 11523     
 
-* How to run the test suite
+3 patients créés :
+ID | FIRST_NAME | LAST_NAME
+---|------------|----------
+1  | Richie     | Zemlak   
+2  | Cristina   | O'Reilly 
+3  | Misha      | Kunde    
 
-* Services (job queues, cache servers, search engines, etc.)
+6 rendez-vous créés :
+ID | DATE                    | DR. ID | DR. FIRSTNAME | DR. LASTNAME | DR. SPECIALTY | DR. ZIP CODE | PATIENT ID | PATIENT FIRSTNAME | PATIENT LASTNAME
+---|-------------------------|--------|---------------|--------------|---------------|--------------|------------|-------------------|-----------------
+1  | 2021-03-30 00:00:00     | 1      | Carmen        | Wehner       | Rhumatologie  | 36614-5292   | 1          | Richie            | Zemlak          
+2  | 2020-08-04 00:00:00     | 2      | Jasmin        | Roob         | Ophtalmologie | 32768-9554   | 2          | Cristina          | O'Reilly        
+3  | 2020-06-17 00:00:00     | 3      | Winnie        | Johnson      | Gynécologie   | 11523        | 3          | Misha             | Kunde           
+4  | 2021-05-09 00:00:00     | 3      | Winnie        | Johnson      | Gynécologie   | 11523        | 1          | Richie            | Zemlak          
+5  | 2021-05-16 00:00:00     | 1      | Carmen        | Wehner       | Rhumatologie  | 36614-5292   | 2          | Cristina          | O'Reilly        
+6  | 2021-05-07 00:00:00     | 2      | Jasmin        | Roob         | Ophtalmologie | 32768-9554   | 3          | Misha             | Kunde           
 
-* Deployment instructions
-
-* ...
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+#Doctor.destroy_all
+#Patient.destroy_all
+#Appointment.destroy_all
+
+specialties_array = ["Psychiatrie", "Gérontologie", "Ophtalmologie", "Gastro-entérologie", "Neurologie", "Gynécologie", "Rhumatologie"]
+doctors_array = []
+patients_array = []
+appointments_array = []
+n = 3
+
+n.times do
+  doctor = Doctor.create!(first_name: Faker::Name.unique.first_name,
+                          last_name: Faker::Name.unique.last_name,
+                          specialty: specialties_array.sample,
+                          zip_code: Faker::Address.unique.zip_code
+                         )
+  doctors_array << doctor
+
+  patient = Patient.create!(first_name: Faker::Name.unique.first_name,
+                            last_name: Faker::Name.unique.last_name
+                           )
+  patients_array << patient
+
+  appointment = Appointment.create!(date: Faker::Date.between_except(from: 1.year.ago, to: n.days.from_now, excepted: Date.today),
+                                    doctor: doctor,
+                                    patient: patient
+                                   )
+  appointments_array << appointment
+end
+
+n.times do |index|
+  appointment = Appointment.create!(date: Faker::Date.forward(days: n + index + 7),
+                                    doctor: doctors_array[(index + n - 1) % n],
+                                    patient: patients_array[(index + n) % n]
+                                   )
+  appointments_array << appointment
+end
+
+require 'table_print'
+
+tp.set Doctor, :id, :first_name, :last_name, :specialty, :zip_code
+tp.set Patient, :id, :first_name, :last_name
+tp.set Appointment, :id, :date, {"doctor.id" => {:width => 6, :display_name => "Dr. Id"}},
+                                {"doctor.first_name" => {:width => 20, :display_name => "Dr. Firstname"}},
+                                {"doctor.last_name" => {:width => 20, :display_name => "Dr. Lastname"}},
+                                {"doctor.specialty" => {:width => 20, :display_name => "Dr. Specialty"}},
+                                {"doctor.zip_code" => {:width => 12, :display_name => "Dr. Zip Code"}},
+                                {"patient.id" => {:width => 10, :display_name => "Patient Id"}},
+                                {"patient.first_name" => {:width => 20, :display_name => "Patient Firstname"}},
+                                {"patient.last_name" => {:width => 20, :display_name => "Patient Lastname"}}
+
+puts "#{n} docteurs créés :\n"
+tp Doctor.all
+puts "\n#{n} patients créés :\n"
+tp Patient.all
+puts "\n#{appointments_array.length} rendez-vous créés :\n"
+tp Appointment.all
+puts
